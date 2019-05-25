503793446aa8a4da8da1d26202f92b63e978f278
James Pier <jpier2012@gmail.com>
2019-05-25 00:44:31 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,6 +4,9 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
+    if req.path.match(/items/)
+      if req.path.split("/items/").last
+
     resp.finish
   end
 
