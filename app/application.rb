645b8886c97a6f0c04b9ae7db3fddeded9de8990
James Pier <jpier2012@gmail.com>
2019-05-25 01:22:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      
+
         binding.pry
       item = Item.item.find do |i|
         i.name == req.path.split("/items/").last
