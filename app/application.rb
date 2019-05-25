2467ebabde2503c48a9efbc0f0fc3997a86aecdb
James Pier <jpier2012@gmail.com>
2019-05-25 00:58:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
     else
-      resp.status
+      resp.write(resp.status)
     end
 
     binding.pry
